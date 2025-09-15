39eb71512c1176cf8a90b9d342a98d7070639c39
Yuktha Reddy Mutchukota <ymutchuk@uoguelph.ca>
2025-09-15 11:44:34 -0400

    updated hello.py with name and hobby

--- hello.py
+++ hello.py
@@ -6,4 +6,6 @@
 # 2. Add another print statement with your name.
 # 3. Add another print statement with your favorite hobby.
 
-print("Hello, world!")
+print ( "Hello, world!" )
+print ( "My name is Yuktha Reddy but you can call me Yuktha or Yuki." )
+print ( "I enjoy anything that can be applied into one's life as a creative outlet! For me, that mainly includes the arts and literature." ) 
